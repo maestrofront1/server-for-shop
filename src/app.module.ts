779eab9421bf94c57c4b540fa5ed4f25116c6862
Maestro3000 <melnik.igor.s@gmail.com>
2025-10-23 00:40:51 +0400

    Add Category and SubCategory services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { SubcategoryModule } from './subcategory/subcategory.module';
 
 
 @Module({
-  imports: [PrismaModule, ProductModule],
+  imports: [PrismaModule, ProductModule, CategoryModule, SubcategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
